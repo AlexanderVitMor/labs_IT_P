56687de20d50344ee5d870e15729673419a4fa94
AlexanderVitMor <alexandr.vitalevichh@gmail.com>
2022-10-19 21:21:29 +0300

    Alpha 1.1 Lab 2

--- IT_P/src/Lab_2/Lab1.java
+++ IT_P/src/Lab_2/Lab1.java
@@ -1,38 +1,44 @@
 package Lab_2;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lab1 {
     public static void main(String[] args){
+
+    // Для удобства использования данных, создаем массив.
         double[] myArray = new double[9];
+
+    // Данный массив служит начальным пакетом хранения координат, которые пользователь вводит с клавиатуры
         for (int i = 0; i < 9; i++){
             Scanner in = new Scanner(System.in);
             double coordinates = in.nextDouble();
             myArray[i] = coordinates;
         }
-
+     // Создание новых экземпляров точек выделение памяти
         Point3d firstPoint = new Point3d(myArray[0], myArray[1], myArray[2]);
         Point3d secondPoint = new Point3d(myArray[3], myArray[4], myArray[5]);
         Point3d thirdPoint = new Point3d(myArray[6], myArray[7], myArray[8]);
 
-        // cd - Closed - замкнутый массив эмитация кольца
+    // cd - Closed - замкнутый массив эмитация кольца
+    // Сюда помещаются все ранее созданные точки
         Point3d[] arrayPointsCd = {firstPoint, secondPoint, thirdPoint, firstPoint};
 
-        if (Point3d.equalityCheck(arrayPointsCd) && equalityCheckMax(arrayPointsCd)) System.out.println("Данный треугольник не существует");
+    // Завершение программы путем вывода нужного результата
+        if (Point3d.equalityCheck(arrayPointsCd) || equalityCheckMax(arrayPointsCd)) System.out.println("Данный треугольник не существует");
         else System.out.println("Площадь треугольника равна: " + computerArea(arrayPointsCd));
 
     }
 
     /**
-     * ываываыва
-     * @param firstPoint ргриигрти
-     * @param secondPoint вапвапвап
-     * @return вапвапв
+     * Метод для нахождения расстояния между любыми двумя точками
+     * @param firstPoint Начальная точка
+     * @param secondPoint Конечная точка
+     * @return Возвращение расстояния от точки до точки с 2 знаками после запятой.
      */
     public static double distanceTo(Point3d firstPoint, Point3d secondPoint)
     {
 
+        // Массивы с координатами
         double[] firstCoordinates = firstPoint.getXYZ();
         double[] secondCoordinates = secondPoint.getXYZ();
         double sumPow = 0;
@@ -43,15 +49,22 @@ public class Lab1 {
         return (double) Math.round(Math.sqrt(sumPow) * 100) / 100;
     }
 
+    /**Метод для нахождения площади треугольника
+     * @param arrayPointsCd передается замкнутый массив с точками
+     * @return площадь треугольника
+     *
+     */
     public static double computerArea(Point3d[] arrayPointsCd){
 
         double semiPerimeter = 0;
 
+        // Массив с расстояниями между точками (стороны треугольника)
         double[] arrayDistance = new double[3];
         for (int i = 0; i < 3; i++) {
             arrayDistance[i] = distanceTo(arrayPointsCd[i], arrayPointsCd[i + 1]);
             semiPerimeter += arrayDistance[i] / 2;
         }
+
 
         double triangleArea = Math.sqrt(semiPerimeter);
 
@@ -62,6 +75,16 @@ public class Lab1 {
         return triangleArea;
     }
 
+    /**Дополнительный метод проверки существования треугольника по сумме сторон
+     * @param arrayPointsCd передается замкнутый массив с точками
+     * @return если выполнилось условия не сущемтвование треуголника возвращается true, иначе false.
+     * Метод проверки: создается двумерный массив 2 строки на 3 столбца,
+     * в первой строке запмсываются поочередные длины сторон AB -> ВС -> CA,
+     * во второй записывается сумма сторон, так что бы под 1 строкой, были разные стороны
+     * Пример:    AB |
+     *         AC + CB
+     * Это сделано для упрощения сравнения в дальнейшем.
+     */
    public static boolean equalityCheckMax(Point3d[] arrayPointsCd){
        double[][] arrayDistance = new double[2][3];
        int maxId = arrayDistance[0].length - 1;
@@ -76,6 +99,7 @@ public class Lab1 {
            }
        }
 
+   // Сравниваются только элементы строк в каждом столбце.
        for (int i = 0; i < 3; i++) {
            if (arrayDistance[0][i] >= arrayDistance[1][i]) return true;
        }
--- IT_P/src/Lab_2/Point3d.java
+++ IT_P/src/Lab_2/Point3d.java
@@ -4,6 +4,14 @@ import java.util.Objects;
 
 public class Point3d extends Point2d{
     private double zCoord;
+
+    /**Описание нового объекта в классе
+     * @param x устанавливается через метод setX()
+     * @param y устанавливается через метод setX()
+     * @param z устанавливается через метод setX()
+     *          Первые два значения созданные в родительском классе приватные,
+     *          Что бы не нарушать политику переменных используем данные методы.
+     */
     public Point3d(double x, double y, double z){
         setX(x);
         setY(y);
@@ -22,10 +30,17 @@ public class Point3d extends Point2d{
         this.zCoord = zCoord;
     }
 
+    /**Дополнительный метод
+     * @return возвращает массив координат точек.
+     */
     public double[] getXYZ(){
         return new double[]{getX(), getY(), getZ()};
     }
 
+    /**Метод проверки объектов (Point3d) на одинаковые координаты
+     * @param arrayPointsCd передается замкнутый массив с точками
+     * @return если точки одинаковые возвращает true, иначе false.
+     */
     public static boolean equalityCheck(Point3d[] arrayPointsCd){
         for (int i = 0; i < 3; i++) {
             if (arrayPointsCd[i].equals(arrayPointsCd[i + 1])) return true;
@@ -33,6 +48,12 @@ public class Point3d extends Point2d{
         return false;
     }
 
+    /**Создание equals для нового класса
+     * @param o в параметры поступает объект для сравнения
+     * @return Что бы предотвратить дубликации кода, переменные созданные
+     * в родительском классе передаются через super.equals по факту
+     * дергают метод описанный уже в родительском классе.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
